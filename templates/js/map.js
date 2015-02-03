31a72f0ab48f7684c06fa4915575972209ea22df
sourenp <souren@papazian.info>
2015-02-03 12:52:38 -0500

    changed folder layout again

---
+++ templates/js/map.js
@@ -0,0 +1,13 @@
+function initialize() {
+  var mapOptions = {
+    center: { lat: 40.807528, lng: -73.962525}, 
+    zoom: 17
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+    },
+    mapTypeId: MY_MAPTYPE_ID
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
