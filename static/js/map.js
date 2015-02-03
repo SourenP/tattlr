930689dd15b43b7f6e3fe5a04b2f8aa50b9a2418
sourenp <souren@papazian.info>
2015-02-03 12:58:38 -0500

    starting map

--- static/js/map.js
+++ static/js/map.js
@@ -2,10 +2,6 @@ function initialize() {
   var mapOptions = {
     center: { lat: 40.807528, lng: -73.962525}, 
     zoom: 17
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
-    },
-    mapTypeId: MY_MAPTYPE_ID
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
