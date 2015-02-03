d88f78fd937d9f6e31f871fd8460f04dd886d035
sourenp <souren@papazian.info>
2015-02-03 12:29:42 -0500

    change layout and added maps

--- routes.py
+++ routes.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 app.config["DEBUG"] = True  # Only include this while you are testing your app
 
 @app.route("/")
-def hello():
-    return "Maps"
+def home():
+    return "Home Page"
+
+@app.route("/map")
+def map():
+    return render_template('index.html')
+
+@app.route('/echo/', methods=['GET'])
+def echo():
+    ret_data = {"value": request.args.get('echoValue')}
+    return jsonify(ret_data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
