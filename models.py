6e335e0871fccf7d3966bd02594f50c3891959d6
Reza Nayebi <rn2324@columbia.edu>
2015-02-04 00:21:11 -0500

    Initial setup for db

---
+++ models.py
@@ -0,0 +1,30 @@
+from routes import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+
+class User(db.Model):
+	__tablename__ = "user"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True)
+	password = db.Column(db.String(30), unique=True)
+
+	def __init__(self, username, message):
+	    self.username = username
+	    self.message = message
+
+	def __repr__(self):
+	    return '<User %r>' % self.username
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(80), unique=True)
+    message = db.Column(sqlalchemy.types.DateTime, unique=True)
+
+    def __init__(self, time, message):
+        self.time = time
+        self.message = message
+
+    def __repr__(self):
+        return '<Post %r>' % self.message
+ 
