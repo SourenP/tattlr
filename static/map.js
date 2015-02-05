71c890baf5dda03c74c985ed109769463094deb6
sourenp <souren@papazian.info>
2015-02-05 15:39:17 -0500

    cleaned code and added your location

--- static/map.js
+++ static/map.js
@@ -1,30 +1,37 @@
+var myLat;
+var myLng;
+var myLatLng;
+var gotCoord = false;
+var styles = [
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      { lightness: 100 },
+      { visibility: "on" }
+    ]
+  },{
+    elementType: "labels.text",
+    stylers: [
+      { visibility: "simplified" }
+    ]
+  },{
+    elementType: "labels.icon",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+  //,{
+  //  stylers: [
+  //    { hue: "#9BDDFF" },
+  //    { saturation: 0 }
+  //  ]
+  //}
+];
+
 function initialize() {
-  var styles = [
-    {
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [
-        { lightness: 100 },
-        { visibility: "on" }
-      ]
-    },{
-      elementType: "labels.text",
-      stylers: [
-        { visibility: "simplified" }
-      ]
-    },{
-      elementType: "labels.icon",
-      stylers: [
-        { visibility: "off" }
-      ]
-    }
-    //,{
-    //  stylers: [
-    //    { hue: "#9BDDFF" },
-    //    { saturation: 0 }
-    //  ]
-    //}
-  ];
+
+  geoFindMe(setMyCoord);
 
   var mapOptions = {
     zoom: 17,
@@ -39,58 +46,73 @@ function initialize() {
   var mapDiv = document.getElementById('map-canvas');
   var map = new google.maps.Map(mapDiv, mapOptions);
 
-  // Create the DIV to hold the control and
-  // call the CenterControl() constructor passing
-  // in this DIV.
+  // Create the input box
   var centerControlDiv = document.createElement('div');
-  var centerControl = new inputBox(centerControlDiv, map);
 
   centerControlDiv.index = 1;
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
-}
 
-function createPost(map, lat, lgn, comment) {
-  var latLng = new google.maps.LatLng(lat, lgn);
-  var marker = new google.maps.Marker({
-        position: latLng,
-        map: map
-      });
-  marker.setVisible(false)
-
-  var label = new Label({ map: map }, comment);
-
-  label.bindTo('position', marker, 'position');
-  label.bindTo('text', marker, 'position');
-}
-
-
-function inputBox(controlDiv, map) {
   var inputBox = document.createElement('input');
   inputBox.className = "inputBox";
   inputBox.type = "text"
 
-  inputBox.onkeypress = 
-    function(evt) {
-      evt = (evt) ? evt : window.event
-      var charCode = (evt.which) ? evt.which : evt.keyCode
-      if (charCode == 13) {
+  // Functionality of input box
+  inputBox.onkeypress = function(evt) {
+    evt = (evt) ? evt : window.event
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode == 13) {
+      if (gotCoord) {
         comment = inputBox.value;
-        console.log(typeof(comment));
-        $.post("/map", {message: JSON.stringify(comment)}, function() {
-          createPost(map, 40.807528, -73.962525, comment);
+        //$.post("/map", {message: JSON.stringify(comment)}, function() {
+          createPost(map, myLat, myLng, comment);
+          map.panTo(myLatLng)
           inputBox.disabled = true;
-        });
-        return false;
+        //});
+      } else {
+        alert("Don't have your location")
       }
-      return true;
-    };
+      return false;
+    }
+    return true;
+  };
 
-  controlDiv.appendChild(inputBox);
+  centerControlDiv.appendChild(inputBox);
+}
 
-  // Setup the click event listeners:
-  //google.maps.event.addDomListener(controlUI, 'click', function() {
-  //  map.setCenter(chicago)
-  //});
+function createPost(map, lat, lng, comment) {
+  var latLng = new google.maps.LatLng(lat, lng);
+  var marker = new google.maps.Marker({ position: latLng, map: map});
+  //marker.setVisible(false)
+
+  var label = new Label({ map: map }, comment);
+  label.bindTo('position', marker, 'position');
+  label.bindTo('text', marker, 'position');
+}
+
+// Geolocation
+
+function geoFindMe(callback) {
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    gotCoord = true;
+    callback(latitude, longitude)
+  };
+
+  function error() {
+    gotCoord = false;
+    alert("Unable to retrieve your location");
+  };
+  
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function setMyCoord(lat, lng) {
+  myLat = lat;
+  myLng = lng;
+  myLatLng = new google.maps.LatLng(lat, lng);
+  console.log("Got Coordinates" + myLat + myLng)
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
