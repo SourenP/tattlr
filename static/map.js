5d8813228dcb4c3b59fbe7964bad0a9dabe11af2
sourenp <souren@papazian.info>
2015-02-07 08:44:53 -0500

    changed color theme

--- static/map.js
+++ static/map.js
@@ -5,37 +5,72 @@ var myLatLng;
 var postDic = {};
 var map_g;
 var styles = [
-  {
-    stylers: [
-      { "lightness": 15 },
-      { "hue": "#0077ff" },
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      { hue: "#0077ff" },
-      { lightness: 100 },
-      { visibility: "simplified" }
-    ]
-  },{
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      { "gamma": 5.01 }
-    ], 
-  },{
-    elementType: "labels.text",
-    stylers: [
-      { visibility: "simplified" }
-    ]
-  },{
-    elementType: "labels.icon",
-    stylers: [
-      { visibility: "off" }
-    ]
-  }
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "lightness": 16
+            },
+            {
+                "hue": "#ff001a"
+            },
+            {
+                "saturation": -61
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "hue": "#ff0011"
+            },
+            {
+                "lightness": 53
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "stylers": [
+            {
+                "hue": "#00ff91"
+            }
+        ]
+    },
+    {
+        "elementType": "labels",
+        "stylers": [
+            {
+                "lightness": 63
+            },
+            {
+                "hue": "#ff0000"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "hue": "#0055ff"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },{
+      elementType: "labels.icon",
+      stylers: [
+        { visibility: "off"}
+      ]
+    }
 ];
 
 
@@ -88,7 +123,7 @@ function initialize() {
         map.setZoom(17);
         inputBox.disabled = true;
       } else alert("Don't have your location");
-    } else if (inputBox.value.length>30) evt.preventDefault();
+    } else if (inputBox.value.length>70) evt.preventDefault();
   };
 
   var logo_div = document.createElement('div');
